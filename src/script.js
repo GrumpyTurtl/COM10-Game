23d2e344eb194e2044005652640df837072c51de
Oli Hills <grumpyTurtl3@proton.me>
2024-05-08 20:59:27 +1200

    added image rotation

--- src/script.js
+++ src/script.js
@@ -1,29 +1,34 @@
 var c = document.getElementById('cvs');
 var ctx = c.getContext('2d');
 
-ctx.canvas.width  = window.innerHeight*1.25;
-ctx.canvas.height = window.innerHeight*0.95;
+ctx.canvas.width = 1000;
+ctx.canvas.height = 900;
+
+//jesus(stack overflow) gave me this in a dream
+ctx.imageSmoothingEnabled = false;
 
 var debugX = 100, debugY = 100;
 function Debug(value){
-    ctx.font = "10px serif"
-    ctx.fillStyle = "white"
+    ctx.font = "10px serif";
+    ctx.fillStyle = "white";
     ctx.fillText(value, debugX,debugY);
     debugX += 10;
 }
 
 function Load(images = []){
     for(let i = 0; i < images.length; i++){
+
         let interval = setInterval(function (){
                 if(images[i].complete){
+                    
                     clearInterval(interval);
                 }
             }
-        ,100)
+        ,1)
     }
 }
 
-
+//very basic SAT that only works on rectangles,
 function checkCollision(a,b){
     //setting initial values
     let aMax = {x:0,y:0};
@@ -81,10 +86,24 @@ function render(a, fillColour) {
     ctx.closePath();
 }
 
+function renderImage(image, x, y, rotation){
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, x, y); // sets scale and origin
+    ctx.rotate(rotation);
+    ctx.drawImage(image, -image.width/2, -image.height/2);
+    ctx.restore();
+} 
+
 
 class GameObject {
     constructor(vertices, image){
         
+    }
+}
+
+class UI{
+    constructor(x,y,w,h,shown,text,button,buttoncall){
+        buttoncall();
     }
 }
 
@@ -111,10 +130,19 @@ car0.src = "imgs/car.png";
 car1.src = "imgs/car1.png";
 car2.src = "imgs/car2.png";
 car3.src = "imgs/car3.png";
+car0.width = 50,car0.height = car0.width* 2;
+car1.width = 50,car1.height = car1.width* 2;
+car2.width = 50,car2.height = car2.width* 2;
+car3.width = 50,car3.height = car3.width* 2;
 
-ctx.fillStyle = "white";
-ctx.fillRect(0,0,c.width,c.height);
-ctx.fillText("Loading", 100,100);
-Load([car1,car2,car3,car0]);
-ctx.fillRect(0,0,c.width,c.height);
-ctx.drawImage(car0,c.width/2 - car0.width/2,c.height/2 - car0.height/2);
+/*the "loading" screen
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,c.width,c.height);
+    ctx.fillText("Loading", 100,100);
+*/
+Load([car1,car2,car3,car0]);//waits for each image to load
+ctx.fillRect(0,0,c.width,c.height);//backgroud
+ctx.drawImage(car0,400,100);
+renderImage(car1,100,100,45);
+renderImage(car0,c.width/2,c.height/2,45);
+ctx.drawImage(car2,400,100);
