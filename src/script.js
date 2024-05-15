eb606a26c0aad61f1334c0ae838fc7a00c2888fb
GrumpyTurtl <GrumpyTurtl3@proton.me>
2024-05-16 10:41:11 +1200

    add comments, fixed delta time, started smothing movement

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
+//links the html canvas to js
 var c = document.getElementById('cvs');
 var ctx = c.getContext('2d');
 
 ctx.canvas.width = 1000;
 ctx.canvas.height = 900;
 
-//jesus(stack overflow) gave me this in a dream
+//jesus(stack overflow) gave me this in a dream, it stops images from becoming blurry when rotated
 ctx.imageSmoothingEnabled = false;
 
+//shows values on screen
 var debugX = 100, debugY = 100;
 function Debug(value){
     ctx.font = "10px serif";
@@ -15,18 +17,19 @@ function Debug(value){
     debugX += 10;
 }
 
+//for every image wait till it loads(currently game is small enough to not need it)
 function Load(images = []){
     for(let i = 0; i < images.length; i++){
 
         let interval = setInterval(function (){
                 if(images[i].complete){
-                    
                     clearInterval(interval);
                 }
             }
         ,1)
     }
 }
+
 
 //very basic SAT that only works on rectangles,
 function checkCollision(a,b){
@@ -56,13 +59,10 @@ function checkCollision(a,b){
     }//note: this is resource heavy, Might be better to save the values to the object, and update them on position change
     //note:its all resource heavy, fix it maybe
 
-   
+   //if it is colliding on the x axis
     if(aMax.x > bMin.x && bMax.x > aMin.x){
-        
+        //and if its colliding on the y axis
         if(aMax.y > bMin.y && bMax.y > aMin.y){
-            // let collisionDepth = {}
-            // collisionDepth.x = (aMax.x > bMin.x) ? aMax.x - bMin.x : bMax.x - aMin.x;
-            // collisionDepth.x = (aMax.y > bMin.y) ? aMax.y - bMin.y : bMax.y - aMin.y;
             return true;
         }else{
             return false;
@@ -72,6 +72,7 @@ function checkCollision(a,b){
     }
 }
 
+//draws every vertex and fills it in
 function render(a, fillColour) {
 
     ctx.lineWidth = 10;
@@ -87,7 +88,7 @@ function render(a, fillColour) {
 }
 
 
-
+//some math stuff that i stole from somewhere ages ago,  used for rotating hitbox,not in use so far, maybe delete it?
 function rotateVertex(vertex, center, angle) {
     const xDiff = vertex.x - center.x;
     const yDiff = vertex.y - center.y;
@@ -98,6 +99,7 @@ function rotateVertex(vertex, center, angle) {
     return { x: xNew, y: yNew };
 }
 
+//also used for roation, and physics later
 function findCenter(vertices){
     const n = vertices.length;
     let sumX = 0;
@@ -236,18 +238,24 @@ car3.width = 100,car3.height = car3.width* 2;
 
 var player = new GameObject([{x:0,y:0},{x:100,y:0},{x:100,y:200},{x:0,y:200}],car0,0.4,{x:0.98, y:0.99},{x:2, y:10});
 
-var roads = [new GameObject([{x:0,y:0}],road,1,0),new GameObject([{x:0,y:0}],road,1,0),]
+var roads = [new GameObject([{x:0,y:0}],road,1,0),new GameObject([{x:0,y:0}],road,1,0),];
+
+var world = {
+    x:0,
+    y:0,
+}
 
 var keybinds = {
-    forward:"w",
-    left:"a",
-    down:"s",
-    right:"d",
+    forward:{key:"w",value:false},
+    left:{key:"a",value:false},
+    down:{key:"s",value:false},
+    right:{key:"d",value:false},
 }
 
 var time = {
     time:0,
-    deltaTime:1,
+    deltaTime: new Date().getTime(),
+    pastTime: new Date().getTime(),
 }
 
 /*the "loading" screen
@@ -259,12 +267,27 @@ var time = {
 Load([car1,car2,car3,car0]);//waits for each image to load
 player.offset(c.width/2 - player.image.width/2,c.height/2 - player.image.height/2);
 roads[0].offset(c.width/2 - roads[0].image.width/2,c.height/2 - roads[0].image.height/2);
-roads[1].goTo(roads[0].x, roads[0].y)
+roads[1].offset(c.width/2 - roads[0].image.width/2,c.height/2 - roads[0].image.height/2 + roads[0].height)
 
 function Loop(){
     ctx.clearRect(0,0,c.width,c.height);
     ctx.fillStyle = "green"
     ctx.fillRect(0,0,c.width,c.height);
+
+    if(roads[0].position.y + roads[0].height < 0){
+        roads[0].offset(0,roads[0].height*2);
+    }
+    if(roads[1].position.y + roads[1].height < 0){
+        roads[1].offset(0,roads[1].height*2);
+    }
+
+    if(roads[0].position.y > c.height){
+        roads[0].offset(0,-roads[0].height*2);
+    }
+    if(roads[1].position.y > c.height){
+        roads[1].offset(0,-roads[1].height*2);
+    }
+
     roads[0].renderImage();
     roads[1].renderImage();
     player.renderImage();
@@ -273,22 +296,29 @@ function Loop(){
 }
 
 window.requestAnimationFrame(Loop);
-setInterval(PhysicsLoop, time.deltaTime);
 
 function PhysicsLoop(){
+    time.deltaTime = new Date().getTime() - time.pastTime;
+    time.pastTime = new Date().getTime();
     player.convertForces();
     player.force.x *= player.friction.x;
     player.force.y *= player.friction.y;
 
     player.offset(player.velocity.x * time.deltaTime,0)
-    roads[0].offset(0,-player.velocity.y);
-    roads[1].offset(0,-player.velocity.y);
+    roads[0].offset(0,-player.velocity.y * time.deltaTime);
+    roads[1].offset(0,-player.velocity.y * time.deltaTime);
+    world.y += -player.velocity.y
 
     time.time += time.deltaTime;
+    window.requestAnimationFrame(PhysicsLoop);
 }
 
+window.requestAnimationFrame(PhysicsLoop);
+
+//i need to make each input change keybind.value, and while it is true repetedly do it
 function inputs(e){
     if(e.key){
+        
         switch(e.key){
             case(keybinds.forward):
                 player.addForce(0,-1 / player.mass);
